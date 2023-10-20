481236fd371475e17ef16023c4584c66a125ab21
charbelbd <147802497+charbelbd@users.noreply.github.com>
2023-10-20 19:16:43 +0300

    my first prjct

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'web1angularr';
+  title = 'xxxxxxx';
+  aa="test123"
+  
 }
